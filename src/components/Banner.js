a2c0ad7854d80eeeefc1e987d7db8ece8b285df6
Andrew Hosegood <andrewhosegood@andrews-imac.home>
2023-10-28 19:17:41 +0100

    final css amends for iphone

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import GearIcon from '../images/gear-solid.svg';
 import BannerOne from '../images/banner-blue.jpg';
@@ -10,13 +10,62 @@ function Banner() {
   const location = useLocation();
   const isPortfolioPage = location.pathname === '/portfolio' || location.pathname === '/portfolio/';
 
+  const [rotation, setRotation] = useState(0);
+
+  
+
+  const [cogPosition, setcogPosition] = useState(0);
+
+  // const [cogPosition, setcogPosition] = useState('absolute');
+
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+
+
+      // if (window.scrollY >= 50) {
+      //   setcogPosition('fixed');
+      // } else {
+      //   setcogPosition('absolute');
+      // }
+
+      // Get the scroll position relative to the entire document
+      const scrollPosition = window.scrollY;
+
+      
+
+      // Calculate the rotation angle based on the scroll position
+      const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+      const rotationAngle = (scrollPosition / maxScroll) * 360;
+
+      // Set the rotation state
+      setRotation(rotationAngle);
+
+      setcogPosition(290);
+
+      
+
+      //console.log(rotationAngle);
+
+    };
+
+    // Add the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
     {isPortfolioPage ? (
 
             <div className={styles.bannerportfolio}>
-            <div className={styles.cog__menu}>
-            <img src={GearIcon} alt="mini-menu" className={styles.cog__img} />
+            <div className={styles.cog__menu} style={{top: cogPosition - rotation }}>
+            <img src={GearIcon} alt="mini-menu" className={styles.cog__img} style={{ transform: `rotate(${rotation}deg)` }} />
             </div>
             <div className={styles.banner__navigation}>
             <span className={styles.banner__navigation1}></span>
@@ -40,7 +89,7 @@ function Banner() {
 
         <div className={styles.banner}>
         <div className={styles.cog__menu}>
-          <img src={GearIcon} alt="mini-menu" className={styles.cog__img} />
+          <img src={GearIcon} alt="mini-menu" className={styles.cog__img} style={{ transform: `rotate(${rotation}deg)` }} />
         </div>
         <div className={styles.banner__navigation}>
           <span className={styles.banner__navigation1}></span>
